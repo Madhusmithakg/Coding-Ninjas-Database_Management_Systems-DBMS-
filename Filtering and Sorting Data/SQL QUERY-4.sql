4d59dc60e6261cfa3c2bff4069a6e363972d3843
Madhusmithakg <96794360+Madhusmithakg@users.noreply.github.com>
2023-04-10 17:42:32 +0530

    SQL QUERY-4

---
+++ Filtering and Sorting Data/SQL QUERY-4.sql
@@ -0,0 +1,31 @@
+/*
+SQL Query - 4
+
+Problem Statement:
+List down all the student's personal details like student id, name and contact information like email, phone number who joined the institute after 2000 and have the surname Daga.
+
+Information about the table:
+Table Student:
+
+Output Table Structure:
++---------+---------+-------+-------------------------+------------+
+| stud_id | fname   | lname | email                   | ph_no      |
++---------+---------+-------+-------------------------+------------+
+
+*/
+
+SELECT stud_id,fname,lname,email,ph_no FROM Student
+WHERE
+joining_year >2000
+AND 
+lname='Daga';
+
+OUTPUT:
+
++---------+---------+-------+-------------------------+------------+
+| stud_id | fname   | lname | email                   | ph_no      |
++---------+---------+-------+-------------------------+------------+
+|     345 | Lokesh  | Daga  | dadspriceloki@yahoo.com | 9988224377 |
+|     334 | Kuldeep | Daga  | kulDaga@yahoo.in        | 7003770037 |
+|     254 | Oscar   | Daga  | oDaga@gmail.com         | 8523455621 |
++---------+---------+-------+-------------------------+------------+
