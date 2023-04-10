0baf7b3df107326fa619d91eb66b61c743514fbb
Madhusmithakg <96794360+Madhusmithakg@users.noreply.github.com>
2023-04-10 17:30:21 +0530

    SQL Query-1

---
+++ Filtering and Sorting Data/SQL Query - 1.sql
@@ -0,0 +1,34 @@
+/*
+Problem Statement:
+List down the student id along with their complete names.
+Information about the table:
+Table Student
+Output Table Structure:
++---------+---------+------------+
+| stud_id | fname   | lname      |
++---------+---------+------------+
+*/
+
+
+
+SELECT stud_id, fname,lname FROM Student;
+
+
+
+OUTPUT:
++---------+---------+------------+
+| stud_id | fname   | lname      |
++---------+---------+------------+
+|     254 | Riya    | Sharma     |
+|     423 | Gautam  | Singh      |
+|     322 | Prem    | Chopra     |
+|     112 | Ash     | Singh      |
+|     345 | Lokesh  | Daga       |
+|     222 | Priya   | Roy        |
+|     262 | Lokesh  | Dhaulakiya |
+|     499 | Sachin  | Kumar      |
+|     196 | Raman   | Kaur       |
+|     334 | Kuldeep | Daga       |
+|     506 | Shivam  | Deka       |
+|     254 | Oscar   | Daga       |
++---------+---------+------------+
